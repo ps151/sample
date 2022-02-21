8e7fb723eba67dec0acd5e227ea39f0303a02d6c
ps151 <gokul151710@gmail.com>
2022-02-21 09:20:37 +0530

    a

--- spam.py
+++ spam.py
@@ -1,11 +1,11 @@
 import time, os, schedule, subprocess
-count = 0
 while True:
+	count = 0
 	f = open("README.md", "a")
 	f.write("a\n")
 	f.close()
 	subprocess.run(["git", "add", "*"])
 	subprocess.run(["git", "commit", "-m", "a"])
 	subprocess.run(["git", "push"])
-	print('==================',count+1,'====================')
+	print('==================',count+11,'====================')
    
